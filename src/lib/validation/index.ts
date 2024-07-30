fd71df8cbcd8d0f7e5b1623dcd187c473df2ef5b
Evan McCall <Xellos1010@Gmail.com>
2024-07-29 20:32:34 -0400

    Sign Up Form Finalized

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,12 @@
+import { z } from "zod"
+
+// TODO: Implement multi-lingual
+export const SignupValidationSchema = z.object({
+    name: z.string().min(2, {message: 'Name must be atleast 2 characters short'}),
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }).max(50),
+    email: z.string().email({ message: 'Invalid email'}),
+    // TODO: Add Password validation for atleast (8 characters, 1 Capitol letter, 1 symbol)
+    password: z.string().min(8, { message: 'Password must be atleast 8 characters'})
+  })
